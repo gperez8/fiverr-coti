8f2e66104b1ae2c5b23282596dc50ed12fc623fd
gperez8 <gregory.facyt@gmail.com>
2018-02-12 00:02:22 -0400

    se hizo que el navbar cambiara de color

--- app/src/js/controllers/appCtrl.js
+++ app/src/js/controllers/appCtrl.js
@@ -1,4 +1,19 @@
 angular.module('app')
 	.controller('appCtrl', function($scope)  { 
-		console.log('HOLA');
+
+		/* Var del controlador */
+			const navbar = $('.navbar');
+			navbar.addClass('navbar-change');
+			const navbarHeight = $('.navbar').height();
+		/* End Var del controlador*/
+
+
+		window.addEventListener('scroll', function(e) {	
+			const navbarCurrentPosition = document.body.scrollTop;
+			if (navbarCurrentPosition >= navbarHeight) {
+				navbar.removeClass('navbar-change');
+			} else {
+				navbar.addClass('navbar-change');
+			}			
+		});
 	});
