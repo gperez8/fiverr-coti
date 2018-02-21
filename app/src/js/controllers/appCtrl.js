06d3a46abbbcaaf35e19f0372cf289f94bf9b0e7
gperez8 <gregory.facyt@gmail.com>
2018-02-20 22:55:59 -0400

    se añadio informacion

--- app/src/js/controllers/appCtrl.js
+++ app/src/js/controllers/appCtrl.js
@@ -125,6 +125,160 @@ angular.module('app')
 
 				]
 			}
+
+
+
+			$scope.faqs = 
+				[ 
+					{
+						category: 'ABOUT COTI',
+						title: 'ABOUT COTI',
+						questions: [
+							{
+								title: 'What is COTI?',
+								answer: 'COTI is a payment transaction network supported by a native digital currency that can process tens-of-thousands of transactions per second. With zero fees for buyers, low-to-zero fees for sellers, and full buyer protection, COTI combines the best of traditional payment methods with the best of digital currencies.'
+							},
+							{
+								title: 'Where is COTI based?',
+								answer: 'COTI is registered in Gibraltar. The development team, investors and advisors are located across various parts of the world.'
+							},
+							{
+								title: 'Who created COTI?',
+								answer: 'COTI was created by a team of payment industry veterans and cryptography experts. For more information on the team, please visit our TEAM page.'
+							}
+
+						]
+					},
+					{
+						category: 'OFFERING',
+						title: 'OFFERING',
+						questions: [
+							{
+								title: 'What problem does COTI solve?',
+								answer: 'The existing payment networks are not suited to deal with new forms of commerce that have emerged over the past few years. While digital currencies like Bitcoin promise lower transaction fees and higher approval rates, they face scaling challenges and low rates of adoption in the context of payments. Moreover, they are not equipped to provide the safeguards (e.g. dispute resolution and fraud protection) that consumers and merchants have grown accustomed to. COTI is building a transparent and secure system that defers governance as much as possible. Most people care more about privacy and transaction costs than they do about non-traceability and anonymity. COTI addresses this demand by providing a payments solution that is faster, easier, more cost-effective and more reliable than existing payment networks.'
+							},
+							{
+								title: 'What is wrong with the current financial system?',
+								answer: 'Liquid assets don’t always match up with outstanding liabilities in global banking systems, and double spending issues are quite prevalent. Censorship is another disadvantage of traditional banking systems, limiting citizen access to information and opportunities. We believe in a world free of credit bubbles and subjective political and moral censorship that can provide advanced banking services with an equitable distribution of risk and fees.'
+							},
+							{
+								title: 'Why will people use COTI rather than existing payments solutions like credit cards?',
+								answer: 'Credit cards rely on infrastructure that was built many years ago by the major card networks. These networks work well when you make a purchase at your local grocery store or on Amazon. However, if you would like to do a cross-border eCommerce transaction (for example, buying something from China) or want to buy a second hand ticket to a football game from another person (peer-to-peer), your credit card will not be as useful. For a cross-border transaction, the transaction fee will be high (3%-5%, not including the FX fee), and the likelihood of you completing the transaction successfully will be low. In many cases, the transaction will be rejected because of issuer/acquirer settings and risk mitigation measures. Moreover, many legitimate businesses operating in problematic industries or jurisdictions are automatically rejected on the grounds of suspected fraud. We believe that buyers and sellers should be evaluated based on their true behaviors over time, not based on broad brush classifications. A seller with an impeccable trust record should pay zero processing fees, while a seller with a less-than-ideal record should incur higher fees.'
+							},
+							{
+								title: 'What are the functions of COTI?',
+								answer: 'COTI provides fast, easy and secure online payment solutions and an intuitive, easy-to-use exchange facility for exchanging COTI’s native currency and other digital/fiat currencies. We aim to streamline transactions between buyers and sellers by taking out the middleman. Compared to credit cards and banks, COTI’s payment network will enable you to make purchases and transfers instantaneously at zero to low fees.'
+							},
+							{
+								title: 'How does COTI plan to become a mainstream payment solution?',
+								answer: 'COTI plans to become widely adopted by offering unique solutions that address the pain points of consumers and merchants, and by providing consumers and merchants with appropriate incentives. In its initial phases, COTI will complement (rather than replace) existing payments solutions.'
+							}
+
+						]
+					},
+					{
+						category: 'COMMUNITY',
+						title: 'COMMUNITY',
+						questions: [
+							{
+								title: 'Which social platforms can I follow COTI on?',
+								answer: 'The existing payment networks are not suited to deal with new forms of commerce that have emerged over the past few years. While digital currencies like Bitcoin promise lower transaction fees and higher approval rates, they face scaling challenges and low rates of adoption in the context of payments. Moreover, they are not equipped to provide the safeguards (e.g. dispute resolution and fraud protection) that consumers and merchants have grown accustomed to. COTI is building a transparent and secure system that defers governance as much as possible. Most people care more about privacy and transaction costs than they do about non-traceability and anonymity. COTI addresses this demand by providing a payments solution that is faster, easier, more cost-effective and more reliable than existing payment networks.'
+							},
+							{
+								title: 'Will updates be sent via email?',
+								answer: 'Yes. You will be notified of all COTI updates following your subscription to our newsletter on the website.'
+							},
+							{
+								title: 'Is there a development team member in the Telegram group?',
+								answer: 'Yes. A member from our development team is available to answer your questions.'
+							},
+							{
+								title: 'Why is there no automated response to frequent questions?',
+								answer: 'We don’t plan on using an automated bot as it’s important for us to interact directly with the COTI community.'
+							},
+							{
+								title: 'How can I participate in the bounty program?',
+								answer: 'If you have a significant following on social media platforms and would like to help us spread the word about our upcoming Token Generation Event (TGE), we’d love for you to join our bounty program in partnership with Bounty0x.Please refer to the following links for full details : \n https://bitcointalk.org/index.php?topic=2273863.msg23054673#msg23054673 \n https://medium.com/@COTInetwork/coti-launches-bounty-program-in-partnership-with-bounty0x-31c85c748525 If you have any questions, you can reach us via email at contact@coti.io.'
+							}
+
+						]
+					},
+					{
+						category: 'INDUSTRY',
+						title: 'INDUSTRY',
+						questions: [
+							{
+								title: 'How does COTI compare to other digital currencies like Ripple?',
+								answer: 'Ripple is a settlement system that is used primarily by financial institutions for interbank settlement purposes. Ripple’s native currency, XRP, was developed to combat transaction spam and is increasingly marketed as a bridge currency between banks. \n COTI is not focused on working with financial institutions, but rather deals primarily in matters relating to consumer payments. COTI was developed from the ground-up to satisfy this consumer payments use case, hence its focus on trust scoring and mediation. COTI’s native currency fuels the COTI network, serves as a means of exchange between buyers and sellers and provides a way to incentivize mediators and levy transaction fees'
+							},
+							{
+								title: 'What makes COTI unique compared to other digital currencies?',
+								answer: 'COTI sets itself apart in two main ways: its Mediation System and Trust Scoring Engine. COTI’s decentralized mediation system provides buyer-seller protections in the event of billing errors, unauthorized charges, or undelivered goods or services. COTIs Trust Scoring Engine rewards highly-trusted users with low-to-zero fees, and levies higher fees on users with a higher risk profile. Additionally, COTI’s high transaction throughput makes it uniquely positioned to accommodate consumer payments at a large scale.'
+							},
+							{
+								title: 'How will you surpass other currencies when there are no immediate and practical advantages for the end user in choosing COTI over other currencies (both digital and fiat)?',
+								answer: 'COTI anticipates that the majority of payments made using the COTI network rails will be denominated in established currencies like the US dollar in the near term. Such currencies naturally benefit from strong existing network effects, and buyers and sellers are more inclined to use them. COTI’s aspiration is for its native currency to evolve into a widely-adopted currency for general payment purposes, thus becoming the Currency Of The Internet. As more people begin to see the advantages of using COTI as a transactional currency, we expect that its adoption will become more widespread. But importantly, COTI’s success does not depend on COTI’s native currency being widely adopted in general payments use cases. COTI’s native currency will always play an important role within the network, even if most transactions are made using other currencies. You can read more about this in section 4 of the Overview Document.'							
+							}
+
+						]
+					},
+					{
+						category: 'TOKEN SALE',
+						title: 'TOKEN SALE',
+						questions: [
+							{
+								title: 'When is the COTI token sale?',
+								answer: 'The date of COTI’s Token Generation Event (TGE) has not been released to the public. Details will soon be released via a mailer to all newsletter subscribers and via various social media channels. The best way to stay up to date is by joining our Telegram group.'
+							},
+							{
+								title: 'How can I purchase COTI tokens?',
+								answer: 'You will be able to purchase COTI tokens using cryptocurrencies (BTC, ETH) or fiat money via bank wires or credit/debit card (Visa or MasterCard).'
+							},
+							{
+								title: 'What is the estimated supply of the COTI token?',
+								answer: 'Full details of COTI’s TGE, including token supply, will be released soon.'
+							},
+							{
+								title: 'Will I receive any bonuses for my contributions in COTI’s TGE?',
+								answer: 'Full details of COTI’s TGE will be released soon.'
+							},
+							{
+								title: 'Are there regulations on your TGE?',
+								answer: 'Full details of COTI’s TGE will be released soon. Before the TGE commences, COTI will provide details of its know-your-customer (KYC) and anti-money laundering (AML) standards, as well as details regarding restricted jurisdictions.'
+							},
+							{
+								title: 'What does it take to become an accredited investor?',
+								answer: 'An accredited investor is a person or entity that can deal with securities not registered with financial authorities by satisfying one of the requirements regarding income, net worth, asset size, governance status or professional experience.'
+							},
+							{
+								title: 'Are there any countries that are restricted from buying COTI?',
+								answer: 'Citizens or residents of the following countries and/or territories can’t participate in our token sale (the list may change): People’s Republic of China, Afghanistan, Bosnia and Herzegovina, Central African Republic, Cuba, Democratic Republic of Congo, Democratic People’s Republic of Korea, Eritrea, Ethiopia, Guinea-Bissau, Iran, Iraq, Israel, Libya, Lebanon, Somalia, South Sudan, Sudan, Syria, Uganda, Vanuatu, Yemen. \n Citizens and residents of United States of America can participate only if they qualify as Accredited Investors.'
+							},
+
+
+						]
+					},
+					{
+						category: 'MEDIATORS',
+						title: 'MEDIATORS',
+						questions: [
+							{
+								title: 'How can I earn money from COTI?',
+								answer: 'To bring about an era of zero friction commerce, the main goal of COTI is to provide the world with a faster, more cost-effective and more reliable way to pay. As an individual, you can help COTI realize this goal by becoming a mediator. Mediators will be compensated in COTI’s native currency for helping with the dispute resolution process. These rewards will be significant, as mediators will play a vital role in maintaining the integrity of the COTI network.'
+							},
+							{
+								title: 'How can I become a mediator?',
+								answer: 'Individuals who would like to become mediators will be required to undergo an online assessment that will help determine if they have the aptitude to perform the mediation tasks at a high standard. Among other requirements, mediators will need to demonstrate satisfactory language proficiency. \n COTI will endeavour to make mediation open to a broad group of people and will offer online training programs that can assist candidates in acquiring the requisite knowledge for contributing to the dispute resolution process. Please refer to section 3.2 in our Overview Document for additional information. Further details on how to become a mediator and how mediators will be incentivized will be released in due course.'
+							}
+
+						]
+					},
+
+				];
+			
+
+			console.log('$scope.faqs', $scope.faqs);
+
 		/* End Var del controlador*/
 
 
